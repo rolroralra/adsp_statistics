febaa95ce5e673322f96ad82902eed86a0259139
Shinyoung Kim <rolroralra@gmail.com>
2020-10-29 15:04:52 +0900

    Update probability_distribution.py

--- workspace/probability_distribution.py
+++ workspace/probability_distribution.py
@@ -15,6 +15,16 @@ import seaborn as sns
 import pandas as pd
 import math
 
+from scipy.stats import binom
+from scipy.stats import poisson
+from scipy.stats import norm
+from scipy.stats import gamma
+from scipy.stats import expon
+
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+
 os.getcwd()
 os.chdir('../data')
 os.getcwd()
@@ -351,3 +361,160 @@ print(f"chi2-statistic: {chi22}")
 print(f"p-value: {p2}")
 print(f"degree of freedom: {dof}")
 print(f"expected_values:\n{expected}")
+print()
+#%%
+######################################################################
+# ANOVA (Analysis of Variance)
+#
+# factor
+# treatment
+######################################################################
+# One Way ANOVA
+######################################################################
+k = treatment_sample_size = 5
+y = [15, 11, 12, 13, 12,
+     18, 17, 16, 17, 16,
+     22, 23, 19, 18, 19]
+# treatments = np.repeat(['A', 'B', 'C'], [5, 5, 5])
+treatments = ['A'] * 5 + ['B'] * 5 + ['C'] * 5
+data = pd.DataFrame({'y': y, 'treatment': treatments})
+print(data)
+print()
+
+sns.boxplot(x='treatment', y='y', data=data, orient='v')
+# sns.boxplot(x='y', y='treatment', data=data, orient='h')
+
+# Ordinary Least Square
+from statsmodels.formula.api import ols
+import statsmodels.api as sm
+
+fitted_linear_model = ols(f'{data.columns[0]} ~ {data.columns[1]}', data = data).fit()
+anova_table = sm.stats.anova_lm(fitted_linear_model)
+print(anova_table)
+print()
+
+print("p-value for ANOVA, Hypothesis is that there is no difference of means for each treatment groups")
+print(": ", anova_table['PR(>F)']['treatment'])
+print()
+#%%
+######################################################################
+# Correlation Analysis
+#
+# 1. Pearson Correlation Coefficient
+# 2. Spearman Correlation Coefficient
+# 3. Kendall's Tau
+######################################################################
+data = pd.read_csv('twins.csv')
+data.head()
+
+x = data[data.columns[0]]
+y = data[data.columns[1]]
+plt.scatter(x, y, label= 'twins data')
+plt.xlabel(x.name)
+plt.ylabel(y.name)
+plt.legend(loc='best')
+
+corr_coef = stats.pearsonr(x, y)
+print("Pearson's Correlation Analysis")
+print(f"Point estimate of Correlation Coefficient r : {round(corr_coef[0], 4)}")
+print(f"p-value : {round(corr_coef[1], 4)}")
+print()
+#%%
+######################################################################
+# Regression Analysis
+#
+# 1. Linear Regression
+#       1) simple regression
+#       2) multiple regression
+#       3) polynomial regression
+#
+# 2. Non-linear Regression  
+######################################################################
+data = pd.read_csv('cars.csv')
+print(data.head(5))
+print()
+
+data = data.drop(data.columns[0], axis=1)
+print(data.head(5))
+print()
+
+x = data[data.columns[0]]
+y = data[data.columns[1]]
+plt.scatter(x, y, label='sccater plot')
+plt.xlabel(x.name)
+plt.ylabel(y.name)
+plt.grid()
+plt.title('scatter plot')
+
+import statsmodels.formula.api as smf
+fitted_lm = smf.ols(f"{y.name} ~ {x.name}", data = data).fit()
+dir(fitted_lm)
+print(fitted_lm.params)
+print()
+
+
+## params
+a = round(fitted_lm.params[0], 3)
+b = round(fitted_lm.params[1], 3)
+print("simple linear regression by OLS (Ordinary Least Square)")
+print(f"y = {a} + {b}x")
+print()
+
+
+## fitted values
+# fitted_value = x.apply(lambda x: a + b * x)
+fitted_values = fitted_lm.fittedvalues
+# fitted_values = fitted_lm.predict(x)
+# predicted_value = fitted_lm.predict({"speed":[1,2,3]})
+
+plt.figure()
+plt.plot(x, fitted_values, '-r', label='fitted value')
+plt.plot(x, y, 'ob', label='observed value')
+plt.xlabel(x.name)
+plt.ylabel(y.name)
+plt.grid()
+plt.legend()
+plt.title('Linear Regression')
+
+
+## residuals
+residuals = fitted_lm.resid
+
+
+print(fitted_lm.summary())
+print()
+
+# evaluating model significance (t, F)
+print("t-values:\n", fitted_lm.tvalues)
+print()
+print("p-value of t:\n", fitted_lm.pvalues)
+print()
+print("F-value :", fitted_lm.fvalue)
+print()
+print("p-value of F:", fitted_lm.f_pvalue)
+print()
+
+# evaluating goodness of fit of model (R-squared : coefficient of determination R^2)
+print("R-squred :", fitted_lm.rsquared)
+print("Adj. R-squared :", fitted_lm.rsquared_adj)
+print()
+
+#%%
+# Residual Analysis
+# 1. Residual Plot 
+plt.figure()
+plt.subplot(1, 2, 1)
+plt.plot(x, residuals, 'o', label='residual')
+plt.title('Residual Plot')
+plt.xlabel(x.name)
+plt.ylabel('Residual')
+plt.tight_layout()
+
+# 2. normal probability plot of residual (QQ Plot : Quantile Quantile Plot)
+# import pylab   # deprecated
+plt.subplot(1, 2, 2)
+stats.probplot(residuals, dist='norm', plot = plt)
+plt.title('QQ Plot')
+plt.ylabel('Sample Quantiles')
+plt.tight_layout()
+#%%
